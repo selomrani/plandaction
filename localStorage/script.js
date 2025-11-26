23408790cbab6522919b3740240b59969ec0af3f
Soufyan <elomranisoufyan@gmail.com>
2025-11-26 14:13:13 +0100

    finished part 2 exercise

--- localStorage/script.js
+++ localStorage/script.js
@@ -1,13 +1,40 @@
-//exercice (partie 1)
-let person = {
-    "name" : "Ali"
-}
-// store to local storage
-const itemtostore = JSON.stringify(person)
-localStorage.setItem("person",itemtostore)
-console.log(itemtostore)
+// //exercice (partie 1)
+// let person = {
+//     "name" : "Ali"
+// }
+// // store to local storage
+// const itemtostore = JSON.stringify(person)
+// localStorage.setItem("person",itemtostore)
+// console.log(itemtostore)
 
-//get from local storage 
-const localdata = localStorage.getItem("person")
-const storeddata = JSON.parse(localdata)
-console.log(storeddata)
+// //get from local storage 
+// const localdata = localStorage.getItem("person")
+// const storeddata = JSON.parse(localdata)
+// console.log(storeddata)
+
+// partie 2
+
+// let people = [{
+//     "name" : "soufyane" , "age" : 19
+// },
+// {
+//     "name" : "adam","age" : 20
+// }]
+
+// store
+
+// const storepeople = JSON.stringify(people)
+// localStorage.setItem("localpeople",storepeople)
+
+// //get
+
+// const getlocal = localStorage.getItem("localpeople")
+// const parseddata = JSON.parse(getlocal)
+// console.log(parseddata)
+
+// remove
+// localStorage.removeItem("localpeople") // shows up as null if you remove the the stored item before trying to get it
+
+
+
+// partie 3
