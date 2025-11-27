9c9de00dbbf7da923e5bbe14a86d29aa750e94d8
Soufyan <elomranisoufyan@gmail.com>
2025-11-27 09:19:12 +0100

    fixed getting data from local storage

--- localStorage/localStoragepart5.js
+++ localStorage/localStoragepart5.js
@@ -2,18 +2,17 @@
 let list = []
 
 const addform = document.forms[0]
-GetLocalStorage("totalpeople")
 addform.addEventListener("submit", (e) => {
-    console.log("works")
     e.preventDefault()
     let person = {
         name: addform.name.value,
         age: addform.age.value
     }
+    GetLocalStorage("totalpeople")
     list.push(person)
     StoreToLocalStorage(list, "totalpeople")
+    console.log(list)
 })
-console.log(list)
 function StoreToLocalStorage(item, key) {
     const ToStore = JSON.stringify(item)
     localStorage.setItem(key, ToStore)
@@ -21,6 +20,6 @@ function StoreToLocalStorage(item, key) {
 function GetLocalStorage(key) {
     let getlocal = localStorage.getItem(key)
     let localdata = JSON.parse(getlocal)
-    localdata = list
+    list = localdata
 }
 
