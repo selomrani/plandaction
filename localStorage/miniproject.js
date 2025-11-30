b1370e89ecba74475e5c95a7cc406f9e716b2c6a
Soufyane <elomranisoufyan@gmail.com>
2025-11-30 18:53:53 +0100

    added function that renders task cards dynamicly

--- localStorage/miniproject.js
+++ localStorage/miniproject.js
@@ -20,7 +20,7 @@ function addnewtask() {
         }
         tasks.push(newtask)
         console.log(tasks)
-        StoreToLocalStorage(tasks,"localtasks")
+        StoreToLocalStorage(tasks, "localtasks")
     })
 }
 addnewtask()
@@ -29,3 +29,30 @@ function StoreToLocalStorage(item, key) {
     const ToStore = JSON.stringify(item)
     localStorage.setItem(key, ToStore)
 }
+
+function RenderTasks() {
+    const taskscontainer = document.getElementById("taskscontainer")
+    taskscontainer.innerHTML = ``
+    const taskcarddy = document.createElement("div")
+    taskcarddy.className = "row g-4 justify-content-center w-100";
+    tasks.forEach((task) => {
+        taskcarddy.innerHTML += `<div class="task-container  col-12 col-md-6 col-lg-3">
+                <div class="task-card d-flex flex-column">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary">${task.title}</h5>
+                        <p class="card-text text-secondary">${task.description}</p>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <button class="btn btn-outline-primary btn-sm btn-action w-45">
+                            <i class="bi bi-pencil-square me-1"></i> Modify
+                        </button>
+                        <button class="btn btn-outline-danger btn-sm btn-action w-45">
+                            <i class="bi bi-trash me-1"></i> Delete
+                        </button>
+                    </div>
+                </div>
+            </div>`
+    })
+    taskscontainer.appendChild(taskcarddy)
+}
+RenderTasks()
