5476ee04d0bf1ea9ae9f3e3c7c286cd4161e73a8
Soufyane <elomranisoufyan@gmail.com>
2025-11-30 18:14:33 +0100

    add function to add new tasks

---
+++ localStorage/miniproject.js
@@ -0,0 +1,25 @@
+let tasks = [
+    {
+        title: "Plan d'action",
+        description: "I need to complete this task by the end of the week",
+
+    },
+    {
+        title: "Brief Project",
+        description: "I need to work on this week's brief project "
+    }
+]
+
+function addnewtask() {
+    const addtaskform = document.forms[0]
+    addtaskform.addEventListener("submit", (e) => {
+        e.preventDefault()
+        let newtask = {
+            title: addtaskform.taskTitle.value,
+            description: addtaskform.taskDescription.value
+        }
+        tasks.push(newtask)
+        console.log(tasks)
+    })
+}
+addnewtask()
