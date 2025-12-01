2333bf1b2d9d8f28f9ba38843ee28e5a0209a244
Soufyan <elomranisoufyan@gmail.com>
2025-12-01 21:57:13 +0100

    added function that deletes the desired object from the array and visually

--- arraysManipulation/index.js
+++ arraysManipulation/index.js
@@ -7,6 +7,7 @@ function addNewContact() {
             fullname: addForm.FullName.value,
             email: addForm.email.value,
             phone: addForm.phone.value,
+            ref: Date.now()
         };
         contacts.push(newContact);
         addForm.reset();
@@ -17,10 +18,10 @@ function addNewContact() {
 addNewContact()
 function RenderContacts() {
     const contactList = document.getElementById("contactList")
+    const contactCard = document.createElement("div")
     contactList.innerHTML = ``
     contacts.forEach((contact) => {
-        const contactCard = document.createElement("div")
-        contactCard.innerHTML = `                    <div class="card mb-3 w-100">
+        contactCard.innerHTML += `                    <div class="card mb-3 w-100">
                         <div class="card-body">
                             <h5 class="card-title fw-bold">${contact.fullname}</h5>
                             <div class="card-text mb-3">
@@ -30,15 +31,27 @@ function RenderContacts() {
                                 <div><i class="bi bi-telephone me-2 text-success"></i>${contact.phone}</div>
                             </div>
                             <div class="d-flex gap-2">
-                                <button class="btn btn-warning flex-fill"><i class="bi bi-pencil-square"></i>
+                                <button class="btn btn-warning flex-fill" data-bs-toggle="modal" data-bs-target="EditContact"><i class="bi bi-pencil-square"></i>
                                     Edit</button>
-                                <button class="btn btn-danger flex-fill"><i class="bi bi-trash"></i> Delete</button>
+                                <button class="deletebtn btn btn-danger flex-fill" data-ref="${contact.ref}"><i class="bi bi-trash"></i> Delete</button>
                             </div>
                         </div>`
         contactList.appendChild(contactCard)
-        console.log(contactList);
-
     })
-
-
+    const deletebtns = document.querySelectorAll(".deletebtn")
+    deletebtns.forEach((deleteBtn) => {
+        deleteBtn.addEventListener("click", (e) => {
+            const currentref = e.target.getAttribute("data-ref")
+            const foundContact = contacts.find((contact) => {
+                return contact.ref == currentref
+            })
+            console.log(foundContact)
+            for (let i = 0; i < contacts.length; i++) {
+                if (contacts[i] == foundContact) {
+                    contacts.splice([i], 1)
+                }
+                RenderContacts()
+            }
+        })
+    })
 }
